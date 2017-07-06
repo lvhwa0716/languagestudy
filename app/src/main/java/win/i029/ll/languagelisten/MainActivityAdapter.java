5a745ca8401d8f45cde7c888a83f8bdf844f7ed5
lvhwa0716 <lvhwa0716@163.com>
2017-07-06 13:51:50 +0800

    Add remove query dialog

--- app/src/main/java/win/i029/ll/languagelisten/MainActivityAdapter.java
+++ app/src/main/java/win/i029/ll/languagelisten/MainActivityAdapter.java
@@ -1,7 +1,10 @@
 package win.i029.ll.languagelisten;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +13,7 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,12 +26,13 @@ public class MainActivityAdapter extends BaseAdapter {
 
     private LayoutInflater mInflater;
     private PlayControl mPlayControl;
-
+    private Context mContext;
 
 
     public MainActivityAdapter(Context context ) {
         this.mInflater = LayoutInflater.from(context);
         mPlayControl = PlayControl.getInstance();
+        mContext = context;
     }
 
 
@@ -58,9 +63,22 @@ public class MainActivityAdapter extends BaseAdapter {
         holder.remove.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("Tag", " position : " + position);
-                mPlayControl.delPlaylistItem(position);
-                MainActivityAdapter.this.notifyDataSetChanged();
+                //Log.d("Tag", " position : " + position);
+                new AlertDialog.Builder(mContext).setMessage(R.string.query_delete)
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // TODO Auto-generated method stub
+                                mPlayControl.delPlaylistItem(position);
+                                MainActivityAdapter.this.notifyDataSetChanged();
+
+                            }
+
+                        })
+                        .setNegativeButton(android.R.string.cancel, null).show();
+
 
             }
         });
